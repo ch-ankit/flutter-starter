0f9d138642cf0d96fca13df1fef198b69eec2539
Ankit Khatiwada <akgunner6@gmail.com>
2022-02-16 23:40:40 +0545

    update: Building cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,53 @@
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
-import './question.dart';
-import './answer.dart';
-import './quiz.dart';
-import './result.dart';
+void main() => runApp(MyHompage());
 
-// void main() {
-//   runApp(const MyApp());
-// }
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  var _totalScore = 0;
-  final _questions = const [
-    {
-      'questionText': 'What\'s your favorite color?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 1},
-      ],
-    },
-    {
-      'questionText': 'What\'s your favorite animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 3},
-        {'text': 'Snake', 'score': 11},
-        {'text': 'Elephant', 'score': 5},
-        {'text': 'Lion', 'score': 9},
-      ],
-    },
-    {
-      'questionText': 'Who\'s your favorite instructor?',
-      'answers': [
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-      ],
-    },
+class MyHompage extends StatelessWidget {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      time: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Grocerries',
+      amount: 16.53,
+      time: DateTime.now(),
+    ),
   ];
-
-  void _answerQuestion(int score) {
-    _totalScore += score;
-    setState(() {
-      _questionIndex += 1;
-    });
-  }
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My first App'),
+          title: Text('Homepage'),
         ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                questionIndex: _questionIndex,
-                questions: _questions,
-                answerQuestion: _answerQuestion,
-              )
-            : Result(_totalScore, _resetQuiz),
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: Container(
+                child: Text(
+                  'Chart',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                width: double.infinity,
+                alignment: Alignment.center,
+              ),
+              color: Colors.blue,
+              elevation: 3,
+            ),
+            Column(children: <Widget>[
+              ...(_transactions
+                  .map((tx) => Card(
+                        child: Text(tx.title),
+                      ))
+                  .toList()),
+            ]),
+          ],
+        ),
       ),
     );
   }
