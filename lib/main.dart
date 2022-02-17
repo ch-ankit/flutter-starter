087a8b07ef8645f9673a304a2be11b2654d121c9
Ankit Khatiwada <akgunner6@gmail.com>
2022-02-17 19:57:46 +0545

    update: Cards added and mapped and controller added for TextFields

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 import 'transaction.dart';
 
 void main() => runApp(MyHompage());
@@ -20,6 +22,7 @@ class MyHompage extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
+    var format = DateFormat('yyyy-MM-dd');
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -39,10 +42,76 @@ class MyHompage extends StatelessWidget {
               color: Colors.blue,
               elevation: 3,
             ),
+            Card(
+              elevation: 2,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(decoration: InputDecoration(labelText: 'Title')),
+                    TextField(decoration: InputDecoration(labelText: 'Amount')),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple)),
+                      child: TextButton(
+                        onPressed: null,
+                        child: Text(
+                          "Add Transaction",
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
             Column(children: <Widget>[
               ...(_transactions
-                  .map((tx) => Card(
-                        child: Text(tx.title),
+                  .map((tx) => Container(
+                        height: 60,
+                        child: Card(
+                          child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.purple, width: 2),
+                                    ),
+                                    padding: EdgeInsets.all(10),
+                                    child: Text('\$${tx.amount}',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                            color: Colors.purple))),
+                                Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      tx.title,
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      format.format(tx.time),
+                                      style: TextStyle(
+                                          fontSize: 10, color: Colors.grey),
+                                    ),
+                                  ],
+                                )
+                              ]),
+                        ),
                       ))
                   .toList()),
             ]),
